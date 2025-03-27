5b2f7ef12f45d96d713bd6aefb3314b3786cfc42
harshit7388 <har.ag7388@gmail.com>
2025-03-27 23:42:11 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Filters from "./components/Filters";
 import '../src/_app.scss'
 import PropertyList from "../src/components/PropertyList";
+import Footer from "./components/Footer";
 const App = () => {
   const [filters, setFilters] = useState({
     location: "",
@@ -17,12 +18,15 @@ const App = () => {
   };
 
   return (
-    <div className="heading">
-      {/* <h1>Real Estate Property Listings</h1> */}
-      <h1>Find <span className="highlight">Perfect</span> Place <br /> To Live Life.</h1>
-      <Filters onFilterChange={handleFilterChange} />
-      <p className="location-notice">Currently available in <span className="location-notice-city">Gurgaon</span> only. Stay tuned for more locations soon. 😊🏡</p>
-      <PropertyList filters={filters}></PropertyList>
+    <div>
+      <div className="heading">
+        {/* <h1>Real Estate Property Listings</h1> */}
+        <h1>Find <span className="highlight">Perfect</span> Place <br /> To Live Life.</h1>
+        <Filters onFilterChange={handleFilterChange} />
+        <p className="location-notice">Currently available in <span className="location-notice-city">Gurgaon</span> only. Stay tuned for more locations soon. 😊🏡</p>
+        <PropertyList filters={filters}></PropertyList>
+      </div>
+        <Footer></Footer>
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaFacebookF, FaLinkedinIn, FaYoutube, FaInstagram } from "react-icons/fa";
+import '../styles/components/_footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-info">
+          <h4>MyBrokers.in</h4>
+          <ul>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-social">
+          <a href="#"><FaFacebookF /></a>
+          <a href="#"><FaLinkedinIn /></a>
+          <a href="#"><FaYoutube /></a>
+          <a href="#"><FaInstagram /></a>
+        </div>
+      </div>
+
+      <div className="footer-legal">
+        <p>© {new Date().getFullYear()} MyBrokers.in. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
