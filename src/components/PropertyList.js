9f0f948545b6d8cb7942b6116477a71a5b660742
harshit7388 <har.ag7388@gmail.com>
2025-04-07 23:41:07 +0530

    changed filter logic for living type,pg

--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -1,91 +1,104 @@
 
-import React, { useState ,useEffect} from 'react';
+import React from 'react';
 import PropertyCard from './PropertyCard';
 import '../styles/components/_propertyList.scss';
 const propertiesData = [
-  {
-    id: 1,
-    title: "Luxury PG with Food",
-    location: "Sector 45",
-    price: "12000",
-    propertyType: "PG with Food",
-    livingType: "For Girls Only",
-    amenities: ["WiFi", "Parking"],
-    images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
-    video: require("../assets/videos/video1.mp4"),
-    ownerName: "Rahul Sharma",
-    ownerPhone: "7388670069",
-  },
-  {
-    id: 2,
-    title: "1 BHK in Ardee City",
-    location: "Ardee City",
-    price: "18000",
-    propertyType: "1BHK",
-    livingType: "Co-living",
-    amenities: ["WiFi", "Furnished"],
-    images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
-    video: require("../assets/videos/video1.mp4"),
-    ownerName: "Rohit Singh",
-    ownerPhone: "7388670069",
-  },
-  {
-    id: 3,
-    title: "2 RK in Sector 57",
-    location: "Sector 57",
-    price: "15000",
-    propertyType: "2 RK",
-    livingType: "For Boys Only",
-    amenities: ["Parking", "Furnished"],
-    // image: "https://picsum.photos/seed/picsum/200/300",
-    images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
-    video: require("../assets/videos/video1.mp4"),
-    ownerName: "Rajesh Kumar",
-    ownerPhone: "7388670069",
-  },
-  {
-    id: 4,
-    title: "PG without Food",
-    location: "Sector 45",
-    price: "10000",
-    propertyType: "PG without Food",
-    livingType: "For Girls Only",
-    amenities: ["WiFi", "Furnished"],
-    images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
-    video: require("../assets/videos/video1.mp4"),
-    ownerName: "Rajat Verma",
-    ownerPhone: "7388670069",
-  },
-  {
-    id: 5,
-    title: "2 BHK Apartment",
-    location: "Sector 57",
-    price: "20000",
-    propertyType: "2 BHK",
-    livingType: "Co-living",
-    amenities: ["WiFi", "Parking", "Furnished"],
-    images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
-    video: require("../assets/videos/video1.mp4"),
-    ownerName: "Rakesh Tiwari",
-    ownerPhone: "7388670069",
-  },
+    {
+      id: 1,
+      title: "Luxury PG",
+      location: "Sector 45",
+      price: "12000",
+      propertyType: "PG",
+      livingType: "For Girls Only",
+      amenities: ["WiFi", "Parking"],
+      images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
+      video: require("../assets/videos/video1.mp4"),
+      ownerName: "Rahul Sharma",
+      ownerPhone: "7388670069",
+    },
+    {
+      id: 2,
+      title: "1 BHK in Ardee City",
+      location: "Ardee City",
+      price: "18000",
+      propertyType: "1 BHK",
+      livingType: "Furnished",
+      amenities: ["WiFi", "Furnished"],
+      images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
+      video: require("../assets/videos/video1.mp4"),
+      ownerName: "Rohit Singh",
+      ownerPhone: "7388670069",
+    },
+    {
+      id: 3,
+      title: "2 RK in Sector 57",
+      location: "Sector 57",
+      price: "15000",
+      propertyType: "2 RK",
+      livingType: "Semi-Furnished",
+      amenities: ["Parking", "Furnished"],
+      // image: "https://picsum.photos/seed/picsum/200/300",
+      images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
+      video: require("../assets/videos/video1.mp4"),
+      ownerName: "Rajesh Kumar",
+      ownerPhone: "7388670069",
+    },
+    {
+      id: 4,
+      title: "PG 1",
+      location: "Sector 45",
+      price: "10000",
+      propertyType: "PG",
+      livingType: "Co-living",
+      amenities: ["WiFi", "Furnished"],
+      images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
+      video: require("../assets/videos/video1.mp4"),
+      ownerName: "Rajat Verma",
+      ownerPhone: "7388670069",
+    },
+    {
+      id: 5,
+      title: "2 BHK Apartment",
+      location: "Sector 57",
+      price: "20000",
+      propertyType: "2 BHK",
+      livingType: "Non-Furnished",
+      amenities: ["WiFi", "Parking", "Furnished"],
+      images:[require("../assets/images/img1.jpg"),require("../assets/images/img2.jpg"),require("../assets/images/img3.jpg")],
+      video: require("../assets/videos/video1.mp4"),
+      ownerName: "Rakesh Tiwari",
+      ownerPhone: "7388670069",
+    },
+    {
+      id: 6,
+      title: "1 RK Studio",
+      location: "Ardee City",
+      price: "11000",
+      propertyType: "1 RK",
+      livingType: "Furnished",
+      amenities: ["WiFi", "Parking"],
+      images: [require("../assets/images/img1.jpg"), require("../assets/images/img2.jpg"), require("../assets/images/img3.jpg")],
+      video: require("../assets/videos/video1.mp4"),
+      ownerName: "Shivam Kumar",
+      ownerPhone: "7388670069",
+    },
 ]
 
 const PropertyList = ({filters}) => {
 
-    const filteredProperties = propertiesData.filter((x)=>{
-        return(
-            (!filters.location || x.location === filters.location)
-            &&
-            (!filters.priceRange || parseInt(x.price.replace(/[^\d]/g, '')) >= parseInt(filters.priceRange.split('-')[0]) && parseInt(x.price.replace(/[^\d]/g, '')) <= parseInt(filters.priceRange.split('-')[1]))
+    const filteredProperties = propertiesData.filter((property) => {
+    const matchLocation =
+    !filters.location || property.location === filters.location;
 
-            &&
-            (!filters.propertyType || x.propertyType === filters.propertyType)
-            &&
-            (!filters.livingType || x.livingType === filters.livingType)
+    const matchPropertyType =
+    !filters.propertyType || property.propertyType === filters.propertyType;
 
-        )
-    });
+    const matchLivingType =
+    !filters.livingType || property.livingType === filters.livingType;
+
+    return matchLocation && matchPropertyType && matchLivingType;
+});
+
     
         // Slick Carousel Settings (Mobile Only)
       const settings = {
