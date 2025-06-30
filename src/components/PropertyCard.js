011ae76c6b9d7bac16f1690b038837576d576f0f
harshit7388 <har.ag7388@gmail.com>
2025-06-30 23:12:44 +0530

    css, code removed,footer  ,price removed from whatsapp message

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -8,7 +8,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import "../styles/components/_propertyCard.scss";
 
 const PropertyCard = ({ property }) => {
-  const message = encodeURIComponent(`Hi, I am interested in your property: ${property.id},${property.title}. My preferrences are: - ${property.location} , ${property.price} , ${property.propertyType} , ${property.livingType}`); ;
+  const message = encodeURIComponent(`Hi, I am interested in your property: ${property.id},${property.title}. My preferrences are: - ${property.location} , ${property.propertyType} , ${property.livingType}`); ;
   const whatsappLink = `https://wa.me/+917388670069?text=${message}`;
   const settings = {
     dots: true,
@@ -17,7 +17,7 @@ const PropertyCard = ({ property }) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false, // Remove next/prev buttons for a clean UI
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
   };
 
@@ -50,7 +50,8 @@ const PropertyCard = ({ property }) => {
       <div className="property-details">
         <h3>{property.title}</h3>
         <div className="location">📍 {property.location}</div>
-        <div className="price">₹ {property.price}</div>
+        {/* <div className="price">₹ {property.price}</div> */}
+        <div className="price priceHidden">₹ Contact for the price</div>
 
         {/* Property Type & Living Type */}
         <div className="property-meta">
@@ -67,14 +68,15 @@ const PropertyCard = ({ property }) => {
 
       {/* Footer (Owner Info & Contact Buttons) */}
       <div className="property-footer">
-        <div className="owner-info">
+        {/* <div className="owner-info">
           <FaUserCircle className="owner-icon" />
           <span className="owner-name">{property.ownerName || "Owner"}</span>
-        </div>
+        </div> */}
         <div className="contact-icons">
           <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
             <FaWhatsapp className="whatsapp-icon" />
           </a>
+          <span className="reachout">Reach out to us</span>
           {/* <a href={`tel:${phone}`}>
             <FaPhone className="phone-icon" />
           </a> */}
